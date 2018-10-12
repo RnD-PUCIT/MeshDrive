785f9dd6f3f86ba4f7b66552f2d25b132e0d67bc
memonasultan32 <bsef15m032@pucit.edu.pk>
2018-10-12 17:50:08 +0500

    removed the commented code

--- FrontEnd/meshdrive-frontend/src/actions/files/downloadFile.js
+++ FrontEnd/meshdrive-frontend/src/actions/files/downloadFile.js
@@ -16,14 +16,7 @@ export default function downloadFile(id) {
       responseType: "blob" // important
     }).then(response => {
       console.log(response);
-
-<<<<<<< HEAD
-      /*  const blob = new Blob([response.data], {type: response.data.type});
-=======
-        console.log(response);
-        
         const blob = new Blob([response.data], {type: response.data.type});
->>>>>>> b82200a9eeade6fff651a5c03b7ec6ce80029576
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
@@ -38,30 +31,9 @@ export default function downloadFile(id) {
         document.body.appendChild(link);
         link.click();
         link.remove();
-<<<<<<< HEAD
-        window.URL.revokeObjectURL(url);*/
-      //___________________________________________________________________________________
-
-      /* const url = window.URL.createObjectURL(new Blob([response.data]));
-=======
         window.URL.revokeObjectURL(url);
-        //___________________________________________________________________________________
-        
-       /* const url = window.URL.createObjectURL(new Blob([response.data]));
->>>>>>> b82200a9eeade6fff651a5c03b7ec6ce80029576
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download',"Bilal.pdf");
-        document.body.appendChild(link);
-        link.click();
-          link.parentElement.removeChild(link)*/
+    
     });
-    /*fetch("https://mysterious-plains-65246.herokuapp.com/downloadFile/"+id)
-      .then(
-
-
-      )
-      .then(d_file => dispatch(downloadFile()))
-      .catch(error => console.log(error));*/
+  
   };
 }
