4a7a88375bde8d5f634d8589a52dc15622a7e0db
mohsinalipro <contactwithmohsin@gmail.com>
2018-10-18 01:16:56 +0500

    Styled login form page

--- FrontEnd/meshdrive-frontend/src/components/pages/Login/Login.js
+++ FrontEnd/meshdrive-frontend/src/components/pages/Login/Login.js
@@ -13,39 +13,41 @@ class Login extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="page" className="d-flex flex-row w-100 page-login">
-          <div className="flex-grow-1 d-flex flex-column pl-4 pr-4 page-content">
-            <h1>Login</h1>
-            <div className="login-signup-forms-wrapper d-flex">
-              <div className="login-form-wrapper">
-                <Form>
-                  <FormGroup>
-                    <Input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="Password"
-                    />
-                  </FormGroup>
+        <div
+          id="page"
+          className="d-flex flex-row w-100 flex-fill page-login justify-content-center align-items-center"
+        >
+          <div className="login-signup-forms-wrapper d-flex align-items-center">
+            <div className="form-wrapper p-5">
+              <h1 className="text-primary mb-4">Log in</h1>
 
-                  <Button>Submit</Button>
-                </Form>
-              </div>
-              <div className="login-signup-form-text">
-                <h2>Have an account?</h2>
-                <p>Login now using your email address and password</p>
-                <a href="#" className="btn btn-primary">
-                  Sign Up
-                </a>
-              </div>
+              <Form className="d-flex flex-column">
+                <FormGroup>
+                  <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </FormGroup>
+
+                <Button className="ml-auto">Login</Button>
+              </Form>
+            </div>
+            <div className="login-signup-form-text p-5">
+              <h2>Have an account?</h2>
+              <p>Login now using your email address and password</p>
+              <a href="#" className="btn btn-outline-light">
+                Sign Up
+              </a>
             </div>
           </div>
         </div>
