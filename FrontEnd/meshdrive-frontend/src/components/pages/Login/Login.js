0154d1e5fdae0e1a291b8dd7a5e0af9ee0dee988
mohsinalipro <contactwithmohsin@gmail.com>
2018-10-18 00:04:07 +0500

    Created login form

--- FrontEnd/meshdrive-frontend/src/components/pages/Login/Login.js
+++ FrontEnd/meshdrive-frontend/src/components/pages/Login/Login.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
-import SideBar from "../../Layout/SideBar/SideBar";
+import "./style.css";
+// import SideBar from "../../Layout/SideBar/SideBar";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 class Login extends Component {
   render() {
     return (
       <React.Fragment>
         <div id="page" className="d-flex flex-row w-100 page-login">
-          <div className="flex-grow-1 d-flex flex-column pl-4 pr-4">
+          <div className="flex-grow-1 d-flex flex-column pl-4 pr-4 page-content">
             <h1>Login</h1>
-            This is login page
-          </div>
+            <div className="login-signup-forms-wrapper d-flex">
+              <div className="login-form-wrapper">
+                <Form>
+                  <FormGroup>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                    />
+                  </FormGroup>
 
-          <SideBar right>Login</SideBar>
+                  <Button>Submit</Button>
+                </Form>
+              </div>
+              <div className="login-signup-form-text">
+                <h2>Have an account?</h2>
+                <p>Login now using your email address and password</p>
+                <a href="#" className="btn btn-primary">
+                  Sign Up
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
