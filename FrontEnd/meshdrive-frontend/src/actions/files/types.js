ff7dcb18e4190d5d6df96b62f9d323f3824aadc0
memonasultan32 <bsef15m032@pucit.edu.pk>
2018-12-04 20:40:53 +0500

    Only API now

--- FrontEnd/meshdrive-frontend/src/actions/files/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_FILES = "FETCH_FILES";
-export const UPLOAD_FILE = "UPLOAD_FILE";
-export const DELETE_FILE = "DELETE_FILE";
-export const DOWNLOAD_FILE = "DOWNLOAD_FILE";
--- FrontEnd/meshdrive-frontend/src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import filesReducer from "./filesReducer";
-import activeFileIdsReducer from "./activeFileIdsReducer";
-
-export default combineReducers({
-  files: filesReducer,
-  activeFileIds: activeFileIdsReducer
-});
