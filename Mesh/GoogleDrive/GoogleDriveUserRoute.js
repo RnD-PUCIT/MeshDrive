bf0ff86ac9fe43ca8e187abc015f63a31139019e
Bilal Yasin <bilalyasin1616@gmail.com>
2019-02-03 17:27:52 +0500

    Added onedrive and googledrive listRootFile to app listRootFiles route

--- Mesh/GoogleDrive/GoogleDriveUserRoute.js
+++ Mesh/GoogleDrive/GoogleDriveUserRoute.js
@@ -386,5 +386,76 @@ router.put('/RenameFile',Constants.checkAccessMiddleware,getGoogleDriveTokensMid
 })
 
 
+module.exports.rootFilesMiddleware=function(req,res,next){
+	req.appCredentials=Constants.GOOGLE_DRIVE_APP_CREDENTIALS;
+	GoogleDriveDAL.readGoogleDriveAccounts(req.userData.email) //Reading user's drive tokens from db
+	.then((accounts)=>{
+		req.googleDriveAccounts=accounts;
+		var listFilesAccount = req.body.listFilesAccount; //List of accounts to get files from
+		var accountTokenList=[];
+		if(req.body.listFilesAccount) //If there is a list of accounts get token for only those accounts
+		{
+			for (let account = 0; account < listFilesAccount.length; account++) { 
+				const givenAccount = listFilesAccount[account];
+				for (let index = 0; index < req.googleDriveAccounts.length; index++) {
+					var storeAccount = req.googleDriveAccounts[index];
+					if(storeAccount.user.emailAddress==givenAccount)
+						accountTokenList.push({email:storeAccount.user.emailAddress,token:storeAccount.token});
+				}
+			}
+		}	
+		else //else get token for all accounts that are in db
+		{
+			for (let index = 0; index < req.googleDriveAccounts.length; index++) {
+				var storeAccount = req.googleDriveAccounts[index];
+				accountTokenList.push({email:storeAccount.user.emailAddress,token:storeAccount.token});
+			}
+		}
 
-module.exports = router;
+		if(accountTokenList.length==0) //If no token found return
+		{
+			next();
+			//return res.status(Constants.CODE_NO_CONTENT).json({message:"No Google Drive account found in user profile."});
+		}
+		else
+		{
+			var promises=[]; //Create array for promises
+			for (let index = 0; index < accountTokenList.length; index++) {
+				const token = accountTokenList[index].token;
+				promises.push(Drive.createAuthOject(req.appCredentials,token)); //call for creating oAuthObjects for each token and push those promises
+			}
+			//Wait for all promises to complete and then go for listing files
+			Promise.all(promises).then(function(oAuth2Clients){
+				var promises=[]; //Array of promises for listing each account files
+				for (let index = 0; index < accountTokenList.length; index++) {
+					var accountEmail=accountTokenList[index].email;
+					promises.push(Drive.listFilesRoot(oAuth2Clients[index],accountEmail)); //call for list file for each account and push those promises
+				}
+				//Wait for all promises to complete and return the response
+				Promise.all(promises).then(function(filesList){
+					res.locals.data=res.locals.data.concat(filesList);
+					next();
+					//res.status(Constants.CODE_OK).json(filesList);
+				}).catch((err)=>{
+					console.log(err);
+					next();
+					//res.status(Constants.CODE_INTERNAL_SERVER_ERROR).json({message:"Error in listing files",err:err});
+				});
+			}).catch((err)=>{
+				console.log(err);
+				next();
+				//res.status(Constants.CODE_INTERNAL_SERVER_ERROR).json({message:"Error in listing files",err:err});
+			});
+			//Only success scenario working
+			//No catch handled here. in case of an error this will not work
+			//Need to check how to handle catch here
+		}
+	})
+	.catch((err)=>{
+		console.log(err);
+		next();
+		//return res.status(Constants.CODE_NO_CONTENT).json({message:"User has no Google Drive accounts",err:err});
+	});
+}
+
+module.exports.router = router;
