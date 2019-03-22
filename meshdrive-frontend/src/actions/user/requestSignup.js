9f27feec57a928280810c82a46b6fd8c17b2aa72
mohsinalipro <contactwithmohsin@gmail.com>
2019-03-23 01:18:17 +0500

    Implemented profile picture

--- meshdrive-frontend/src/actions/user/requestSignup.js
+++ meshdrive-frontend/src/actions/user/requestSignup.js
@@ -1,6 +1,6 @@
 // module imports
 import React from "react";
-import axios from "axios";
+import axios, { post } from "axios";
 
 // custom module imports
 import { REQUEST_LOGIN } from "./types";
@@ -16,66 +16,67 @@ export const signup = response => {
   };
 };
 
-export default function requestSignup(email, password, name) {
+export default function requestSignup(email, password, name, profilePic) {
   return dispatch => {
     console.log("Dispatching startApiRequest from requestLogin");
     dispatch(startApiRequest());
 
-    axios
-      .post(apiRoutes.users.signup, {
-        email,
-        password,
-        name
-      })
-      .then(
-        response => {
-          const statusCode = response.status;
+    const formData = new FormData();
 
-          let responseUiComponent;
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("name", name);
+    if (profilePic) formData.append("profilePic", profilePic, profilePic.name);
 
-          switch (statusCode) {
-            case 200:
-              responseUiComponent = (
-                <SweetAlertWrapper success title="Success">
-                  {response.data.message}
-                </SweetAlertWrapper>
-              );
-              break;
-            case 400:
-              responseUiComponent = (
-                <SweetAlertWrapper danger title="Fail">
-                  {response.data.error}
-                </SweetAlertWrapper>
-              );
-              break;
-          }
+    axios.post(apiRoutes.users.signup, formData).then(
+      response => {
+        const statusCode = response.status;
 
-          console.log("Dispatching finishApiRequest from requestSignup succes");
+        let responseUiComponent;
 
-          dispatch(finishApiRequest(response, true, responseUiComponent));
-          dispatch(signup(response));
-        },
-        error => {
-          // const statusCode = error.response.status;
-          const defaultErrorMessage =
-            "Error, Something went wrong. Please try again";
-
-          const responseUiComponent = (
-            <SweetAlertWrapper danger title="Fail">
-              {error &&
-              error.response &&
-              error.response.data &&
-              error.response.data.message
-                ? error.response.data.message
-                : error.response.data.error
-                ? error.response.data.error
-                : defaultErrorMessage}
-            </SweetAlertWrapper>
-          );
-          console.log("Dispatching finishApiRequest from requestLogin error");
-
-          dispatch(finishApiRequest(error.response, true, responseUiComponent));
+        switch (statusCode) {
+          case 200:
+            responseUiComponent = (
+              <SweetAlertWrapper success title="Success">
+                {response.data.message}
+              </SweetAlertWrapper>
+            );
+            break;
+          case 400:
+            responseUiComponent = (
+              <SweetAlertWrapper danger title="Fail">
+                {response.data.error}
+              </SweetAlertWrapper>
+            );
+            break;
         }
-      );
+
+        console.log("Dispatching finishApiRequest from requestSignup succes");
+
+        dispatch(finishApiRequest(response, true, responseUiComponent));
+        dispatch(signup(response));
+      },
+      error => {
+        // const statusCode = error.response.status;
+        const defaultErrorMessage =
+          "Error, Something went wrong. Please try again";
+
+        const responseUiComponent = (
+          <SweetAlertWrapper danger title="Fail">
+            {error &&
+            error.response &&
+            error.response.data &&
+            error.response.data.message
+              ? error.response.data.message
+              : error.response.data.error
+              ? error.response.data.error
+              : defaultErrorMessage}
+          </SweetAlertWrapper>
+        );
+        console.log("Dispatching finishApiRequest from requestLogin error");
+
+        dispatch(finishApiRequest(error.response, true, responseUiComponent));
+      }
+    );
   };
 }
